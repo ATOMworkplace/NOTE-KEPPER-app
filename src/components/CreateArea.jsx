eb3e4aa0e53b28bb28f8b347485705d88700987c
/Charchit_Gangwar/ <114564628+ATOMworkplace@users.noreply.github.com>
2023-12-21 21:14:01 +0530

    Add files via upload

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [expand, setexpand] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  function expandBox() {
+    setexpand(true);
+  }
+  return (
+    <div>
+      <form className="create-note">
+        {expand && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          onClick={expandBox}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={expand ? 3 : 1}
+        />
+        <Zoom in={expand}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AttachFileIcon from "@material-ui/icons/CollectionsBookmark";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AttachFileIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
